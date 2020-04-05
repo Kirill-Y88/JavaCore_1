e719e1e50f4747d234a6d47effb5cac57b6a1d49
KirillYrevichPushkin <benberov09@mail.ru>
2020-04-05 23:49:46 +0300

    Ветка Homework1 Добавлены классы Класс BackGround нерабочий. Функцию изменения цвета фона реализовал в GameCanvas.

---
+++ src/Lesson1/BackGround.java
@@ -0,0 +1,22 @@
+package Lesson1;
+
+import java.awt.*;
+
+public class BackGround  {
+    private  float time;
+    private Color color;
+
+    BackGround(){
+
+
+    }
+
+    public void settime(float time){
+        this.time = time;
+    }
+    public void info(){
+        System.out.println(time);
+    }
+
+
+}
